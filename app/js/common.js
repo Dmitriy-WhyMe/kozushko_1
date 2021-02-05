46b3b81d9095572799376d01b1644a1a33bf41e1
Dmitriy <dima.ivahnoff@mail.ru>
2021-02-05 17:09:33 +0300

    complete 70% need menu responsive 1.5 slayders font

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,19 @@
 (function ($) {
 	$(document).ready(function () {
-
-
+		
+		$('.single-item').slick({
+			dots: true,
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			adaptiveHeight: true
+		  });
+		$('.multiple-items').slick({
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 3
+		});
 	});
 })(jQuery);
+
+
