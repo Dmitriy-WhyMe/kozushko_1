3b510e5c108e3f5f922490ef2bf541cbe53fba25
Dmitriy <dima.ivahnoff@mail.ru>
2021-02-08 10:54:59 +0300

    fonts_1

--- dist/js/common.js
+++ dist/js/common.js
@@ -11,7 +11,10 @@
 		$('.multiple-items').slick({
 			infinite: true,
 			slidesToShow: 3,
-			slidesToScroll: 3
+			slidesToScroll: 3,
+			arrows: true,
+			prevArrow: $('.arrow_prev'),
+			nextArrow: $('.arrow_next')
 		});
 	});
 })(jQuery);
