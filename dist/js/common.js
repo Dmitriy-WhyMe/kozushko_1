e3d8a0023d3592863a56b6dbf60846296bdbf589
Dmitriy <dima.ivahnoff@mail.ru>
2021-02-09 13:23:18 +0300

    menu_style+dorabot_respons_2_slayders

--- dist/js/common.js
+++ dist/js/common.js
@@ -13,6 +13,7 @@
 			slidesToShow: 3,
 			slidesToScroll: 3,
 			arrows: true,
+			adaptiveHeight: true,
 			prevArrow: $('.arrow_prev'),
 			nextArrow: $('.arrow_next')
 		});
@@ -22,6 +23,7 @@
 			slidesToScroll: 1,
 			arrows: false,
 			fade: true,
+			adaptiveHeight: true,
 			asNavFor: '.slider-nav'
 		  });
 		  $('.slider-nav').slick({
@@ -29,9 +31,44 @@
 			slidesToScroll: 1,
 			asNavFor: '.slider-for',
 			focusOnSelect: true,
+			adaptiveHeight: true,
 			prevArrow: $('.arrow_prev_1'),
 			nextArrow: $('.arrow_next_1')
 		  });
+
+
+
+		  $(function() {
+			$('#main-menu').smartmenus({
+			  mainMenuSubOffsetX: -1,
+			  mainMenuSubOffsetY: 4,
+			  subMenusSubOffsetX: 6,
+			  subMenusSubOffsetY: -6
+			});
+		  });
+		  
+		  // SmartMenus mobile menu toggle button
+		  $(function() {
+			var $mainMenuState = $('#main-menu-state');
+			if ($mainMenuState.length) {
+			  // animate mobile menu
+			  $mainMenuState.change(function(e) {
+				var $menu = $('#main-menu');
+				if (this.checked) {
+				  $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+				} else {
+				  $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+				}
+			  });
+			  // hide mobile menu beforeunload
+			  $(window).bind('beforeunload unload', function() {
+				if ($mainMenuState[0].checked) {
+				  $mainMenuState[0].click();
+				}
+			  });
+			}
+		  });
+		
 	});
 })(jQuery);
 
